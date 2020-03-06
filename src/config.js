3cf000aee7352f5bce4bc166851851b88f57e244
Liam Dyer <liamcdyer@gmail.com>
2020-03-06 17:01:18 -0500

    Babel

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   size: 4,
   count: 50000,
-  updatesPerFrame: 100, // Must be a multiple of the count
+  updatesPerFrame: 1000, // Must be a multiple of the count
   speed: 0.1,
   speedRandomness: 0.03
 }
