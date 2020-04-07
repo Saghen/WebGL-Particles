519ce2bcc6388e799e06517b287b259e27b9abe1
Liam Dyer <liamcdyer@gmail.com>
2020-04-07 17:01:22 -0400

    Config update for preview

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
+// TODO: Validate config
 export default {
-  size: 8,
-  count: 100,
-  updatesPerFrame: 50, // Must be a multiple of the count
+  size: 1,
+  count: 1000000,
+  updatesPerFrame: 100000, // Must be a multiple of the count
   speed: 0.1,
   speedRandomness: 0.03
 }
